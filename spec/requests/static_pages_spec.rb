62338b6bd2325cfaba65147b4e8195e625fb6c2f
Bill437403423 <bill_roddick@hotmail.com>
2014-09-28 21:30:35 -0500

    final after contact

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  
+  let(:base_title) {"ruby on rails"}
 
   describe "Home page" do
 
@@ -10,7 +12,7 @@ describe "Static pages" do
     end
     it "should have the title 'ruby on rails|home'" do
     visit '/static_pages/home'
-    expect(page).to have_title('ruby on rails|home')
+    expect(page).to have_title("#{base_title}|home")
   end
   end
 
@@ -22,7 +24,7 @@ describe "Static pages" do
     end
     it "should have the title 'ruby on rails|help'" do
     visit '/static_pages/help'
-    expect(page).to have_title('ruby on rails|help')
+    expect(page).to have_title("#{base_title}|help")
   end
   end
   
@@ -33,8 +35,18 @@ describe "Static pages" do
 	  end
 	  it "should have the title 'ruby on rails|about'" do
     visit '/static_pages/about'
-    expect(page).to have_title('ruby on rails|about')
+    expect(page).to have_title("#{base_title}|about")
   end
   end
-   
+  
+ describe "Contact Us"do
+	it "should have the content 'contact'" do
+	       visit '/static_pages/contact'
+       expect(page).to have_content('contact')
+	end
+ it "should have the title 'ruby on rails|contact'" do
+ visit '/static_pages/contact'
+expect(page).to have_title("#{base_title}|contact")
+ end
+ end
 end	
