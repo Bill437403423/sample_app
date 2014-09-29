6219c34e9ad57ba15ebdbe0554321680eb89c648
Bill437403423 <bill_roddick@hotmail.com>
2014-09-28 20:58:11 -0500

    final

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+    it "should have the title 'ruby on rails|home'" do
+    visit '/static_pages/home'
+    expect(page).to have_title('ruby on rails|home')
+  end
+  end
+
+  describe "Help page" do
+     
+    it "should have the content 'Help'" do
+       visit '/static_pages/help'
+       expect(page).to have_content('Help')
+    end
+    it "should have the title 'ruby on rails|help'" do
+    visit '/static_pages/help'
+    expect(page).to have_title('ruby on rails|help')
+  end
+  end
+  
+  describe "About us" do
+	  it "should have the content 'About Us'" do
+		  visit '/static_pages/about'
+		  expect(page).to have_content('About Us')
+	  end
+	  it "should have the title 'ruby on rails|about'" do
+    visit '/static_pages/about'
+    expect(page).to have_title('ruby on rails|about')
+  end
+  end
+   
+end	
